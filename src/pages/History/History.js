831586ce93857f92092508d1f3d54a7d95973e84
Lucas Vitro <lucasvitro9@gmail.com>
2022-09-13 23:58:15 -0400

    history

---
+++ src/pages/History/History.js
@@ -0,0 +1,19 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+
+function History() {
+  return (
+    <Card style={{ width: '18rem' }}>
+      <Card.Img variant="right" src="https://sanctuarywellnessinstitute.com/cannabis/images/cannabis-card-logo.png" />
+      <Card.Body>
+        <Card.Title>Cannabis</Card.Title>
+        <Card.Text>
+          Historiazinha pipipi popopo
+        </Card.Text>
+        
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default History;
